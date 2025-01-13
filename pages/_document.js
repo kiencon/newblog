c679304ae85d9b7cbaef7e02add8df651e8d4d91
VietSonBui <bui00091@algonquinlive.com>
2025-01-13 17:29:43 -0500

    init project

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='en' className='theme-compiled dark'>
+        <Head />
+        <body
+          className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
+        >
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
